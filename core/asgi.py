2fc8b51deaec5609a527aa6b41c62f5c142db5d3
YevhenYalovenko <jenyaya0007@gmail.com>
2023-07-20 13:43:21 +0300

    add base, homework. Finish work on home_page

--- core/asgi.py
+++ core/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for homework_10__yevhen__yalovenko project.
+ASGI config for core project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -12,7 +12,7 @@ import os
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "homework_10__yevhen__yalovenko.settings"
+    "DJANGO_SETTINGS_MODULE", "core.settings"
 )
 
 application = get_asgi_application()
